f8a794bfbb7cd06748d5e5595b2f6cace59de759
Travis Swift <travis.arthur.swift@gmail.com>
2022-02-23 09:46:22 -0600

    added console log in js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,26 @@
-window.alert("This is an alert! JavaScript is running!");
-window.alert("this is another aler! What's up!!!!");
+// this created a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+
+// WHat is this?
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this sill do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("the fight has begun!");
+}
+
+// fight();
+
+window.alert(playerName);
+
+
+
+    
+    
